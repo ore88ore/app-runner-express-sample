d43a64a79b45d4b92182b3d44c65eebb8a210209
Yusuke Sakai <sakai.yusuke@classmethod.jp>
2021-06-04 00:05:48 +0900

    feat: postgresql イメージの追加と接続処理を追加

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = 8000;
+
+const initOptions = {};
+const pgp = require("pg-promise")(initOptions);
+const cn = {
+  host: "localhost",
+  port: 5432,
+  database: "sample",
+  user: "postgres",
+  password: "ppostgres",
+  max: 10,
+};
+const db = pgp(cn);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/products", async (req, res) => {
+  const products = await db.any("SELECT * FROM products");
+  res.send(JSON.stringify(products));
 });
 
 app.listen(port, () => {
